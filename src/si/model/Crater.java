d0074de6cad28e9d2c3c0f1a879af0ee835b58ff
Leo <960015420@qq.com>
2023-10-22 18:05:55 +0800

    correct

--- src/si/model/Crater.java
+++ src/si/model/Crater.java
@@ -2,11 +2,19 @@ package si.model;
 
 import javafx.scene.paint.RadialGradient;
 
-public record Crater(RadialGradient gradient, double[] x_coords_detail, double[] y_coords_detail, int numVertices) {
-    public RadialGradient getGradient() {
-        return gradient;
-    }
+public class Crater {
+    private RadialGradient gradient;
+    private double[] x_coords_detail;
+    private double[] y_coords_detail;
+    private int numVertices;
 
+    public Crater(RadialGradient gradient, double[] x_coords_detail, double[] y_coords_detail, int numVertices) {
+        this.gradient = gradient;
+        this.x_coords_detail = x_coords_detail;
+        this.y_coords_detail = y_coords_detail;
+        this.numVertices = numVertices;
+    }
+    public void setGradient(RadialGradient gradient) { this.gradient = gradient; }
     public double[] getX_coords_detail() {
         return x_coords_detail;
     }
