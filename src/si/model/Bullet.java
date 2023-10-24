c3f0fcf8ee37c34f1b6c1dcd1feb2c3071b2df81
Leo <960015420@qq.com>
2023-10-24 12:20:19 +0800

    change the bullet speed to 8

--- src/si/model/Bullet.java
+++ src/si/model/Bullet.java
@@ -11,7 +11,7 @@ public class Bullet implements Movable, Hittable {
     private static int bulletCounter = 0;
     public static final int BULLET_HEIGHT = 4;
     public static final int BULLET_WIDTH = 2;
-    private static final int BULLET_SPEED = 5;
+    private static final int BULLET_SPEED = 8;
     private String type;
 
     public Bullet(double x, double y, double rotation, String type) {
